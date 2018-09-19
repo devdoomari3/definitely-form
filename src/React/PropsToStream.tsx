0be3b5d18dae393ef2a165a2a69f5f7007b8384c
Dev Hanmari <devtest1mari@gmail.com>
2018-09-19 19:09:15 +0900

    add-react-propsToStream-StreamToComponentProps
    .

--- src/React/PropsToStream.tsx
+++ src/React/PropsToStream.tsx
@@ -2,17 +2,49 @@ import * as React from 'react';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 
-export type PropsType<T> = {
-  value: T;
-  children(stream: Observable<T>): React.ReactElement<any>;
-};
-export class PropsToStream<T> extends React.Component<PropsType<T>> {
-  stream: BehaviorSubject<T> = new BehaviorSubject(this.props.value);
+export type PropsType<ValueType, OriginalPropsType> = {
+  mapper(
+    originalProps: Readonly<OriginalPropsType>,
+  ): ValueType;
+  children(stream: Observable<ValueType>): React.ReactElement<OriginalPropsType>;
+} & OriginalPropsType;
+
+export class PropsToStream<
+  ValueType,
+  OriginalPropsType,
+> extends React.Component<PropsType<ValueType, OriginalPropsType>> {
+  // mapper: (originalProps: OriginalPropsType) => ValueType;
+  stream: BehaviorSubject<ValueType> =
+    new BehaviorSubject(this.props.mapper(this.props));
 
   componentDidUpdate() {
-    this.stream.next(this.props.value);
+    this.stream.next(this.props.mapper(this.props));
   }
   render() {
     return this.props.children(this.stream);
   }
+}
+
+export function propsToStream<Value, OriginalProps>(
+  mapper: (props: Readonly<OriginalProps>) => Value,
+) {
+  type StreamProps = {
+    stream?: Observable<Value>;
+  };
+
+  return <P extends OriginalProps & StreamProps>(
+    Component: React.ComponentType<P> | React.StatelessComponent<P>,
+  ) => {
+    return (props: Readonly<OriginalProps>) => (
+      <PropsToStream
+        mapper={mapper}
+        {...props}
+      >
+        {stream => (
+
+          <Component {...props} stream={stream} />
+        )}
+      </PropsToStream>
+    );
+  };
 }
