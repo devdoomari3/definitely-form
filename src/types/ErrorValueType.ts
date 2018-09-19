0e06610bea13a56b45977485bc32079fceeeb0ac
Dev Hanmari <devtest1mari@gmail.com>
2018-09-19 10:20:45 +0900

    move to derived-stream
    .

--- src/types/ErrorValueType.ts
+++
@@ -1,9 +0,0 @@
-import { FormSpecBase } from './FormSpecBase';
-
-export type BaseErrorValuesType<FormSpec extends FormSpecBase> = {
-  [key in keyof FormSpec]: any;
-};
-
-export type DefaultErrorValuesType<FormSpec extends FormSpecBase> = {
-  [key in keyof FormSpec]: string;
-};
