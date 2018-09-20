efc6e4522cba4decdef55ccdd3148d1f94cb0fd8
devdoomari.circleci <devdoomari@gmail.com>
2018-09-20 03:48:36 +0000

    generated from:ed3f751082e65349becfaf1fdc76658d838f6aa1 triggered by:devdoomari3 [ci skip]

---
+++ lib/FormState.d.ts
@@ -0,0 +1,14 @@
+import { FormData, FormDataParsed } from './types/FormData';
+import { FormSpecBase } from './types/FormSpecBase';
+export declare type FormState<FormSpec extends FormSpecBase> = {
+    active: keyof FormSpec | null;
+    touched: {
+        [key in keyof FormSpec]?: boolean;
+    };
+    edited: {
+        [key in keyof FormSpec]?: boolean;
+    };
+    values: Partial<FormData<FormSpec>>;
+    parsedValues: Partial<FormDataParsed<FormSpec>>;
+    isInvalid?: boolean;
+};
