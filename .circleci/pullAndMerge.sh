7945b0e92ec3713e49a282d52063eec09e9f9555
Dev Hanmari <devtest1mari@gmail.com>
2018-10-02 17:10:03 +0900

    [circleci] fix pushing artefacts to master
    [circleci] fix git pull error due to conflict
    
    [circleci] build after merge
    
    [circleci] fix circleci mistake
    
    [circleci] fix circleci mistake

--- .circleci/pullAndMerge.sh
+++ .circleci/pullAndMerge.sh
@@ -1,6 +1,7 @@
 git pull origin $CIRCLE_BRANCH
 git merge -X theirs --no-edit $CIRCLE_BRANCH
+npm run build
 git add -f lib docs
-git config user.email "devdoomari@gmail.com" && git config user.name "devdoomari.circleci"
+mkdir ~/.git && git config user.email "devdoomari@gmail.com" && git config user.name "devdoomari.circleci"
 git commit --allow-empty -m $'generated from:'"$CIRCLE_SHA1"$'\ntriggered by:'"$CIRCLE_USERNAME"$'\n[ci skip]'
 git tag -a $'dist_'"$CIRCLE_BRANCH"'_'"$CIRCLE_BUILD_NUM" -m "."
